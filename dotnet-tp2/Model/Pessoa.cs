37d3c6cd293f0c96f9cb3b2626512266f9cb9727
Matheus Piraine <matheuspiraine@MacBook-Pro-de-Matheus.local>
2024-03-10 19:17:05 -0300

    Commit inicial

---
+++ dotnet-tp2/Model/Pessoa.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace dotnet_tp2.Model
+{
+    public class Pessoa
+    {
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        [StringLength(100, ErrorMessage = "O nome não pode ter mais de 100 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A idade é obrigatória.")]
+        [Range(0, 150, ErrorMessage = "A idade deve estar entre 0 e 150.")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "O CPF é obrigatório.")]
+        [RegularExpression(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$", ErrorMessage = "Formato inválido de CPF.")]
+        public string CPF { get; set; }
+
+        public Pessoa(string nome, int idade, string cpf)
+        {
+            Nome = nome;
+            Idade = idade;
+            CPF = cpf;
+        }
+    }
+
+
+}
+
---
+++ dotnet-tp2/Pages/PessoaPage.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dotnet_tp2.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnet_tp2.Pages
+{
+	public class PessoaPageModel : PageModel
+    {
+        public List<Pessoa> Pessoas { get; set; }
+
+        public void OnGet()
+        {
+            Pessoas = new List<Pessoa>
+            {
+                new Pessoa("João", 30, "123.456.789-00"),
+                new Pessoa("Maria", 25, "987.654.321-00"),
+                new Pessoa("José", 40, "111.222.333-44")
+            };
+        }
+    }
+}
